2bdbcd9079d6a2beb184e8d1fb0432d230d98341
triplee12 <tripleeoliver@gmail.com>
2022-06-09 16:35:57 -0700

    Add Idea Class Base View

--- src/crudl/crudl/apps/ideas/urls.py
+++ src/crudl/crudl/apps/ideas/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
 from .views import (
-    idea_list,
+    # IdeaList,
+    # idea_list,
+    IdeaListView,
     IdeaDetail,
     add_or_change_idea,
     delete_idea
 )
 
 urlpatterns = [
-    path("", idea_list, name="idea_list"),
+    path("", IdeaListView.as_view(), name="idea_list"),
     path("add/", add_or_change_idea, name="add_idea"),
     path("<uuid:pk>/", IdeaDetail.as_view(), name="idea_detail"),
     path("<uuid:pk>/change/", add_or_change_idea, name="change_idea"),
