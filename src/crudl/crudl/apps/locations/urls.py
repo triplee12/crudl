9d2dc1f5ae0ff39fe8031fabc39cf2c1d526f07f
triplee12 <tripleeoliver@gmail.com>
2022-06-17 04:35:14 -0700

    Implement Modal Preview

--- src/crudl/crudl/apps/locations/urls.py
+++ src/crudl/crudl/apps/locations/urls.py
@@ -4,4 +4,5 @@ from .views import LocationList, LocationDetail
 urlpatterns = [
     path("", LocationList.as_view(), name="location_list"),
     path("<uuid:pk>/", LocationDetail.as_view(), name="location_detail"),
+    path("<uuid:pk>/modal/", LocationDetail.as_view(template_name="locations/location_detail_modal.html"), name="location_detail_modal"),
 ]
--- src/crudl/crudl/crudl_static/site/js/list.js
+++ src/crudl/crudl/crudl_static/site/js/list.js
@@ -8,3 +8,26 @@ jQuery(function ($) {
         contentSelector: '.item,.pagination'
     });
 });
+
+jQuery(function ($) {
+    var $list = $('.item-list');
+    var $modal = $('#modal');
+    $modal.on('click', '.close', function (event) {
+        $modal.modal('hide');
+        // Do something when dialog is close
+    });
+    $list.on('click', 'a.item', function (event) {
+        var $link = $(this);
+        var url = $link.data('modal-url');
+        var title = $link.data('modal-title');
+        if (url && title) {
+            event.preventDefault();
+            $('.modal-title', $modal).text(title);
+            $('.modal-body', $modal).load(url, function () {
+                $modal.on('shown.bs.modal', function () {
+                    // Do something when dialog is shown
+                }).modal('show');
+            });
+        }
+    });
+});
